406ff2be56f6aba891acfea4d405a7e0665f3e71
George Olufemi <georgeoluolufemi@gmail.com>
2022-06-09 17:06:00 +0100

    Installed 3D Framework and Integrated 3D deps-

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
     <React.Fragment>
-      <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='h-screen w-screen overflow-hidden flex-col justify-center relative'>
+        <div className="">
+          <Spline scene="https://prod.spline.design/byrJM8w93RVNc-k9/scene.splinecode" />
+        </div>
+      </div>
+      <div>hello</div>
     </React.Fragment>
   );
 }
